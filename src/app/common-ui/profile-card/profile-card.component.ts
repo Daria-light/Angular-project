fab98efa4ca2803622546fe9279913adcc5b9ab9
Daria <lampochkat@bk.ru>
2025-08-25 09:51:51 +0300

    provide Http + Pipe for img-url

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Profile } from '../../data/interface/profile-interface';
+import { CommonModule } from '@angular/common';
+
+import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
 
 @Component({
   selector: 'app-profile-card',
-
-  imports: [],
+  imports: [CommonModule, ImgUrlPipe],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss',
 })
-export class ProfileCardComponent {}
+export class ProfileCardComponent {
+  @Input() profile!: Profile;
+}
