dffc507b5b36d99fd6bcf0d0611473d267d30398
Daria <lampochkat@bk.ru>
2025-08-31 10:34:57 +0300

    layout page + beta login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
-import { ProfileService } from './data/services/profile.service';
 import { CommonModule } from '@angular/common';
-import { Profile } from './data/interface/profile-interface';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +9,4 @@ import { Profile } from './data/interface/profile-interface';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'nik-talk';
-  profileService = inject(ProfileService);
-
-  profiles: Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccount().subscribe((val) => {
-      this.profiles = val;
-    });
-  }
-}
+export class AppComponent {}
---
+++ src/app/pages/search-page/search-page.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { ProfileService } from '../../data/services/profile.service';
+import { Profile } from '../../data/interface/profile-interface';
+import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from '../../common-ui/profile-card/profile-card.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-page',
+  imports: [RouterOutlet, ProfileCardComponent, CommonModule],
+  templateUrl: './search-page.component.html',
+  styleUrl: './search-page.component.scss',
+})
+export class SearchPageComponent {
+  title = 'nik-talk';
+  profileService = inject(ProfileService);
+
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccount().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
+}
