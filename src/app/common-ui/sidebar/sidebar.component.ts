782cf9201c5a668356ba7705dbff83898a4041eb
Daria <mattveychuk.dasha@gmail.com>
2025-09-12 11:30:35 +0300

    FINAL sidebar menu icons

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 import { IconComponent } from '../svg-icon/svg-icon.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [IconComponent],
+  imports: [IconComponent, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: '',
+    },
+    {
+      label: 'Чаты',
+      icon: 'chats',
+      link: 'chats',
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search',
+    },
+  ];
+}
