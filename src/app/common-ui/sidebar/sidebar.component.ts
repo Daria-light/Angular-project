608895766738a904639d0db7d0029d4e591547b4
Daria <mattveychuk.dasha@gmail.com>
2025-09-14 22:04:30 +0300

    feat(sidebar): implement subscribers data loading from API

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { IconComponent } from '../svg-icon/svg-icon.component';
 import { CommonModule } from '@angular/common';
+import { SubscriberCardComponent } from './subscriber-card/subscriber-card.component';
+import { ProfileService } from '../../data/services/profile.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [IconComponent, CommonModule],
+  imports: [IconComponent, CommonModule, SubscriberCardComponent, RouterLink],
+  providers: [ProfileService],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  profileService = inject(ProfileService);
+
+  subscribers$ = this.profileService.getSubscribersShotList();
+
+  // me = this.profileService.me;
+
   menuItems = [
     {
       label: 'Моя страница',
--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Profile } from '../interface/profile-interface';
+import { ReturnStatement } from '@angular/compiler';
+import { Pageble } from '../interface/pageble.interface';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +15,12 @@ export class ProfileService {
 
   getTestAccount() {
     return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`);
+  }
+
+  getSubscribersShotList() {
+    return this.http
+      .get<Pageble<Profile>>(`${this.baseApiUrl}account/subscribers/`)
+      .pipe(map((res) => res.items.slice(3)));
   }
 
   getMe() {
